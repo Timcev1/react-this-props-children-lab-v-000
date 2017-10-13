c9dffd79081525019398902e1dea268009b17c24
Timothy Cevallos <timcev@gmail.com>
2017-10-13 03:40:09 +0000

    automatically backed up by learn

--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -1,15 +1,3 @@
 // Code ThemedDecoration Component Here
 import React from 'react';
 import PropTypes from 'prop-types';
-
-class Invitation extends React.Component {
-  render(){
-    return (
-      <div>
-      <h1>You've been invited!</h1>
-      {this.props.children}
-      </div>
-    )
-  }
-}
-export default Invitation
